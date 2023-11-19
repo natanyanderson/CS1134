b680d19b9dc2e7adec51ff6a0d018908b8008dc1
natanyanderson <146006938+natanyanderson@users.noreply.github.com>
2023-11-19 13:20:31 -0500

    Add files via upload
    I am a Computer And Electrical Engineering student and NYU Tandon. Here are my homeworks/labs and in class code for CS 1134 Data Structures and Algorithms.

---
+++ LinkedBinaryTree - Copy (1).py
@@ -0,0 +1,39 @@
+from ArrayQueue import ArrayQueue
+
+class LinkedBinaryTree:
+    class Node:
+        def __init__(self, data, left=None, right=None):
+            self.data = data
+            self.left = left
+            if (left is not None):
+                self.left.parent = self
+            self.right = right
+            if (right is not None):
+                self.right.parent = self
+            self.parent = None
+
+
+    def __init__(self, root=None):
+        self.root = root
+        self.size = self.count_nodes()
+
+    def __len__(self):
+        return self.size
+
+    def is_empty(self):
+        return (len(self) == 0)
+
+
+    def count_nodes(self):
+        def subtree_count(root):
+            if(root is None):
+                return 0
+            else:
+                left_count = subtree_count(root.left)
+                right_count = subtree_count(root.right)
+                return left_count + right_count + 1
+
+        return subtree_count(self.root)
+
+
+
